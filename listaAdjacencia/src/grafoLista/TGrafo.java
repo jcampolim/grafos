c8d46f7d575d139be2810c8b9f6b1eb3c6384b3f
jcampolim <julia.campolim@gmail.com>
2025-03-03 13:06:55 -0300

    Adicionando exercícios 21 e 24 + correção dos métodos de inserir e remover aresta

--- listaAdjacencia/src/grafoLista/TGrafo.java
+++ listaAdjacencia/src/grafoLista/TGrafo.java
@@ -1,5 +1,8 @@
 package grafoLista;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class TNo {
 	public int w;      // vértice que é adjacente ao elemento da lista
 	public TNo prox;
@@ -16,21 +19,21 @@ public class TGrafo {
 	    this.m = 0;
 	    TNo[] aux = new TNo[n];
 	    
-		for(int i = 0; i< n; i++)
+		for(int i = 0; i < n; i++)
 			aux[i]=null;
 
 	    this.adj = aux;
 	}
 
 	// insere uma aresta v->w no grafo, onde v e w são distintos, positivos e menores que n
-	public void insereA( int v, int w) {
+	public void insereA(int v, int w) {
 	    TNo novoNo;
 
 	    TNo no = adj[v];
 	    TNo ant = null;
 
 		// anda na lista para chegar ao final
-	    while(no != null && w >= no.w){
+	    while(no != null){
 	        if(w == no.w)
 	            return;
 	        ant = no;
@@ -43,7 +46,7 @@ public class TGrafo {
 	    novoNo.prox = no;
 
 	    // atualiza a lista
-	    if(ant == null){
+	    if(ant == null) {
 	        adj[v] = novoNo;      // insere no inicio
 	    } else {
 			ant.prox = novoNo;    // insere no final
@@ -53,31 +56,81 @@ public class TGrafo {
 	}
 
 	// remove a aresta v->w, onde v e w são distintos, positivos e menores que n
-	public void removeA( int v, int w) {
+	public void removeA(int v, int w) {
 	    // obtém o início da lista do vértice v
 	    TNo no = adj[v];
 	    TNo ant = null;
 
+		System.out.println(no.w != w);
+
 	    // percorre a lista do vértice v procurando w (se adjacente)
-	    while( no != null && no.w != w ){
+	    while(no != null && no.w != w) {
 			ant = no;
+
+			if(no.prox == null) break;
+
 			no = no.prox;
 	    }
 
-	    // se w é adjacente, remove da lista de v
-	    if (no != null){
-			if (ant != null) {
-				ant.prox = no.prox;
+		if(no != null && ant == null) {
+			if(no.prox == null) {
+				adj[v] = null;
+			} else {
+				adj[v] = adj[v].prox;
 			}
-			no = null;
+
+			m--;
+
+			return;
+		}
+
+	    // se w é adjacente, remove da lista de v
+	    if(no != null) {
+			ant.prox = no.prox;
+	    	no = null;
 	    	m--;
-		}	
+		}
+	}
+
+	// TODO: 19 - calcula o grau de entrada de um vértice
+	public int inDegree(int v) {
+		return 0;
+	}
+
+	// TODO: 20 - calcula o grau de saída de um vértice
+	public int outDegree(int v) {
+		return 0;
+	}
+
+	// 21 - calcula o grau de um vértice
+	public int degree(int v) {
+		return inDegree(v) + outDegree(v);
+	}
+
+	// 24 - inverte a ordem dos vértices adjacêntes na lista de adjacência
+	public void inveteOrdem(int v) {
+		List<Integer> nosAdjacentes = new ArrayList<>();
+
+		TNo proxNo = adj[v];
+
+		// percorre a lista de nós, removendo-os e armazenando-os
+		while(proxNo != null) {
+			nosAdjacentes.add(proxNo.w);
+			removeA(v, proxNo.w);
+
+			proxNo = proxNo.prox;
+		}
+
+		// adiciona os nós na lista na ordem inversa
+		for(int i = nosAdjacentes.size() - 1; i >= 0; i--) {
+			insereA(v, nosAdjacentes.get(i));
+		}
 	}
 
 	// apresenta o grafo contendo número de vértices, arestas e a lista de adjacência obtida
 	public void show() {
 	    System.out.println("n: " + n);
-	    System.out.println("m: " + m + "\n");
+	    System.out.println("m: " + m);
 
 	    for(int i = 0; i < n; i++){
 	    	System.out.print("\n" + i + ": ");
